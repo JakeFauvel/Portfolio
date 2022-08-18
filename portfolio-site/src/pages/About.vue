c4171befe2b3ba736820498f7d735d03c8bd649e
Jake Fauvel <jakefauvel@msn.com>
2022-08-18 22:31:11 +0200

    About page updates

--- portfolio-site/src/pages/About.vue
+++ portfolio-site/src/pages/About.vue
@@ -6,7 +6,7 @@
       <h1 class="about-title">Hey, I'm Jake.</h1>
       <div class="about-info">
         <p>I am a University of Brighton graduate with a degree in Digital Media Development (BSc), I achieved first class honours.</p>
-        <p>My current role is the Lead Developer / Head of Web Development at <a href="https://www.bricoflor.de/" target="_blank" style="background: #6F1A35; color: #FFF;">BRICOFLOR GmbH</a>, based in Wuppertal Germany. I am responsible for all of the companies technical aspects, from internal tools to the companies production websites.</p>
+        <p>My current role is the Lead Developer / Head of Web Development at <a href="https://www.bricoflor.de/" target="_blank" style="background: #6F1A35; color: #FFF;">BRICOFLOR GmbH</a>, based in Wuppertal Germany. I am responsible for all of the companies technical aspects, from internal tools to the companies production ecommerce websites. We have websites in all of the major European countries.</p>
         <p>A large part of my role is actively developing new features, creating workflows/requirements, documentation and leading a small team of developers.</p>
         <p>I specialise in full-stack development and the following technologies:</p>
 
@@ -17,18 +17,18 @@
             <li>PHP</li>
             <li>JavaScript</li>
             <li>HTML, CSS + Sass</li>
-            <li>Mobile App Development</li>
+            <li>Magento 2</li>
+            <li>Laravel</li>
             <li>Bootstrap</li>
-            <li>Infrastructure, Nginx, Varnish + more</li>
           </ul>
 
           <ul class="block-two">
+            <li>Infrastructure, Nginx, Varnish, pm2</li>
+            <li>Mobile App Development</li>
             <li>Documenting, testing and planning</li>
-            <li>Magento 2</li>
-            <li>Laravel</li>
             <li>API's / AJAX</li>
             <li>Relational databases</li>
-            <li>Atlassian suite, Git, AWS</li>
+            <li>Atlassian Suite, Git, AWS</li>
             <li>Cordova</li>
             <li>Phaser.io</li>
           </ul>
@@ -37,7 +37,7 @@
         My previous roles include: <a href="https://community.musictribe.com/" target="_blank" style="background: #DA0402; color: #FFF;">Music Tribe</a> in Willich, Germany. Creating hybrid mobile applications for their mixing consoles.
 
         <p>Before that, I was working at <a href="https://tillo.io" target="_blank" style="background: #5854E5; color: #FFF;">Tillo</a> in Brighton & Hove, United Kingdom. The main part of my role was developing new features for their Sass web application / API <service class=""></service></p>
-        <p>Want to know more or see a full list of my skills? <a href="" style="color: #FFF; cursor: pointer;">Take a look at my CV</a> or my <a href="" style="color: #FFF; cursor: pointer;">personal projects.</a></p>
+        <p>Want to know more or see a full list of my skills? <a href="" style="color: #FFF; cursor: pointer;">Take a look at my CV (coming soon)</a> or my <a href="/projects" style="color: #FFF; cursor: pointer;">personal projects.</a></p>
       </div>
     </div>
   </Layout>
