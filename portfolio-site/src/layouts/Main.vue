f78d923e7d43d74e5f9d474cd25ff452dfb1c371
Jake Fauvel <jakefauvel@msn.com>
2022-08-24 14:23:49 +0200

    Basic cookie consent policy

--- portfolio-site/src/layouts/Main.vue
+++ portfolio-site/src/layouts/Main.vue
@@ -25,7 +25,31 @@
     },
     data () {
       return {
-        hamburgerClicked: false
+        hamburgerClicked: false,
+        consentManager: null
+      }
+    },
+    mounted() {
+      let ccCookieStatus = localStorage.getItem('cc_cookie');
+      let ccAccepted = localStorage.getItem('cc_accepted');
+      console.log(ccCookieStatus)
+      console.log(ccAccepted)
+      if (typeof window.initCookieConsent !== 'function' && ccCookieStatus === null && !ccAccepted) {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.defer = true;
+        script.async = true;
+        document.head.appendChild(script);
+        script.addEventListener("load", () => {
+          this.cookieConsentLoaded();
+        });
+        script.src = 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v2.8.5/dist/cookieconsent.js';
+
+        const cssFile = document.createElement('link');
+        cssFile.type = 'text/css';
+        cssFile.rel = 'stylesheet';
+        cssFile.href = 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@v2.8.5/dist/cookieconsent.css';
+        document.head.appendChild(cssFile);
       }
     },
     methods: {
@@ -36,11 +60,93 @@
         } else {
           this.$refs.mainLayout.style.overflow = 'initial';
         }
+      },
+      cookieConsentLoaded() {
+        document.body.classList.add('c_darkmode');
+        this.consentManager = window.initCookieConsent();
+        this.loadConsentManager();
+      },
+      loadConsentManager() {
+        this.consentManager.run({
+          current_lang: 'en',
+          autoclear_cookies: false,
+          page_scripts: true,
+          hide_from_bots: true,
+          gui_options: {
+            consent_modal: {
+              layout: 'box',
+              position: 'bottom left',
+              transition: 'slide'
+            },
+            settings_modal: {
+              layout: 'bar',
+              position: 'right',
+              transition: 'slide'
+            }
+          },
+          onFirstAction: function(user_preferences, cookie) {
+            // Callback triggered only once
+          },
+          onAccept: function (cookie) {
+            localStorage.setItem('cc_accepted', true);
+          },
+          onChange: function (cookie, changed_preferences) {
+            // ...
+          },
+          languages: {
+            'en': {
+              consent_modal: {
+                title: 'Just to let you know',
+                description: 'In order to see how many people have visited my site, I use Google Analytics. None of the data is personally identifiable but I just wanted to make you aware.',
+                primary_btn: {
+                  text: 'Acknowledge',
+                  role: 'accept_all'
+                }
+              },
+              settings_modal: {
+                title: 'Cookie usage 🍪',
+                save_settings_btn: 'Save settings',
+                accept_all_btn: 'Accept all',
+                blocks: [
+                  {
+                    description: 'We would like to use some additional services and cookies on the website to enhance your online experience. You can choose for each category to opt-in/out whenever you want. Some cookies are not optional for functional purposes. For more details relating to cookies and other sensitive data, please read the full <a href="/privacy" class="cc-link">privacy policy</a>.'
+                  }, {
+                    title: 'Strictly necessary cookies',
+                    description: 'These cookies are essential for the proper functioning of my website. Without these cookies, the website would not work properly.',
+                    toggle: {
+                      value: 'necessary',
+                      enabled: true,
+                      readonly: true
+                    }
+                  }, {
+                    title: 'Performance and Analytics cookies',
+                    description: 'These cookies allow the website to remember the choices you have made in the past. They also allow us to see the number of views and optimise content.',
+                    toggle: {
+                      value: 'analytics',
+                      enabled: true,
+                      readonly: true
+                    }
+                  }, {
+                    title: 'Advertisement and Targeting cookies',
+                    description: 'These cookies collect information about how you use the website, which pages you visited and which links you clicked on, if a purchase was made and so on! All of the data is anonymized and cannot be used to identify you.',
+                    toggle: {
+                      value: 'targeting',
+                      enabled: true,
+                      readonly: false
+                    }
+                  }, {
+                    title: 'More information',
+                    description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="/contacts">contact us</a>.'
+                  }
+                ]
+              }
+            }
+          }
+        })
       }
-    }
+    },
   }
 </script>
-
 <static-query>
 query {
   metadata {
@@ -87,6 +193,13 @@ query {
   }
   nav {
     z-index: 1000;
+  }
+  #cm {
+    background: #212227 !important;
+    color: #FFF !important;
+  }
+  #cm #c-inr #c-bns button {
+    background: #FFF !important;
   }
 </style>
 <style lang="scss" scoped>
